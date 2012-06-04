9446c388a6ecb7ccb2fb321d8b03e718e2536cec
Allan Jardine <allan.jardine@sprymedia.co.uk>
2012-06-04 16:51:49 +0100

    Bootstrap integration update: Use fluid width rows Bootstrap integration update: Add the sorting images for the header sorting options. We can't use the Bootstrap icons because we need additional HTML for that (an <I> tag), but DataTables 1.10 (or possibly 1.11) will add a good deal of functionality in that regard.

--- integration/bootstrap/dataTables.bootstrap.js
+++ integration/bootstrap/dataTables.bootstrap.js
@@ -1,6 +1,6 @@
 /* Set the defaults for DataTables initialisation */
 $.extend( true, $.fn.dataTable.defaults, {
-	"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+	"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
 	"sPaginationType": "bootstrap",
 	"oLanguage": {
 		"sLengthMenu": "_MENU_ records per page"
