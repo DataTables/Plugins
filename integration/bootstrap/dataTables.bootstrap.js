e29eebeaeeffb5cb96b6b3215184e2090d82ec25
Allan Jardine <allan.jardine@sprymedia.co.uk>
2012-06-06 12:57:54 +0100

    Add TableTools integration to the Bootstrap integration files

--- integration/bootstrap/dataTables.bootstrap.js
+++ integration/bootstrap/dataTables.bootstrap.js
@@ -105,3 +105,36 @@ $.extend( $.fn.dataTableExt.oPagination, {
 		}
 	}
 } );
+
+
+/*
+ * TableTools Bootstrap compatibility
+ * Required TableTools 2.1+
+ */
+if ( $.fn.DataTable.TableTools ) {
+	// Set the classes that TableTools uses to something suitable for Bootstrap
+	$.extend( true, $.fn.DataTable.TableTools.classes, {
+		"container": "btn-group",
+		"buttons": {
+			"normal": "btn",
+			"disabled": "btn disabled"
+		},
+		"collection": {
+			"container": "DTTT_dropdown dropdown-menu",
+			"buttons": {
+				"normal": "",
+				"disabled": "disabled"
+			}
+		}
+	} );
+
+	// Have the collection use a bootstrap compatible dropdown
+	$.extend( true, $.fn.DataTable.TableTools.DEFAULTS.oTags, {
+		"collection": {
+			"container": "ul",
+			"button": "li",
+			"liner": "a"
+		}
+	} );
+}
+
