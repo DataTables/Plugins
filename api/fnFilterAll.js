cb7b8ac3bbf521bc889203e2e432c87719bada97
Allan Jardine <allan.jardine@sprymedia.co.uk>
2012-07-01 17:21:58 +0100

    Fix: fnFilterAll was a bit dodgy - 10670.

--- api/fnFilterAll.js
+++ api/fnFilterAll.js
@@ -17,19 +17,10 @@
  *    });
  */
 
-$.fn.dataTableExt.oApi.fnFilterAll = function(oSettings, sInput, iColumn, bRegex, bSmart) { 
-	if ( typeof bRegex == 'undefined' ) {
-		bRegex = false;
-	}
-	 
-	if ( typeof bSmart == 'undefined' ) {
-		bSmart = true;
-	}
-	 
-	for (var i in this.dataTableSettings) {
-		jQuery.fn.dataTableExt.iApiIndex = i;
-		this.fnFilter(sInput, iColumn, bRegex, bSmart);
-	}
-
-	jQuery.fn.dataTableExt.iApiIndex = 0;
+$.fn.dataTableExt.oApi.fnFilterAll = function(oSettings, sInput, iColumn, bRegex, bSmart) {
+    var settings = $.fn.dataTableSettings;
+    
+    for ( var i=0 ; i<settings.length ; i++ ) {
+      settings[i].oInstance.fnFilter( sInput, iColumn, bRegex, bSmart);
+    }
 };
