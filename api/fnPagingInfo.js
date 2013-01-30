38fa0524444d278802629211092f6f6cac5adb6b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-01-30 07:42:15 +0000

    api - fnPagingInfo.js plug-in didn't correctly handle -1 page length

--- api/fnPagingInfo.js
+++ api/fnPagingInfo.js
@@ -18,13 +18,15 @@
 
 $.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
 {
-  return {
-	"iStart":         oSettings._iDisplayStart,
-	"iEnd":           oSettings.fnDisplayEnd(),
-	"iLength":        oSettings._iDisplayLength,
-	"iTotal":         oSettings.fnRecordsTotal(),
-	"iFilteredTotal": oSettings.fnRecordsDisplay(),
-	"iPage":          Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
-	"iTotalPages":    Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
-  };
+	return {
+		"iStart":         oSettings._iDisplayStart,
+		"iEnd":           oSettings.fnDisplayEnd(),
+		"iLength":        oSettings._iDisplayLength,
+		"iTotal":         oSettings.fnRecordsTotal(),
+		"iFilteredTotal": oSettings.fnRecordsDisplay(),
+		"iPage":          oSettings._iDisplayLength === -1 ?
+			0 : Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
+		"iTotalPages":    oSettings._iDisplayLength === -1 ?
+			0 : Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
+	};
 };
