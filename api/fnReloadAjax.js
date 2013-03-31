46df5ba4bc7c0d4b4f0a3b7c57c51ad3786064da
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-03-31 08:44:46 +0100

    Fix: fnReloadAjax's standing redraw was totally broken. Fixed here.
    - Problem was that fnDraw( false ) was being called, on new data, which
      meant that no filtering or sorting was being applied. Need to call
      fnDraw() first, and then fnDraw( false ) to reposition the pages. Not
      ideal, but works for now.
    
    - Worth noting that `ajax.reload()` will be a method in DT 1.10's API.

--- api/fnReloadAjax.js
+++ api/fnReloadAjax.js
@@ -10,14 +10,14 @@
  *  @example
  *    // Example call to load a new file
  *    oTable.fnReloadAjax( 'media/examples_support/json_source2.txt' );
- *     
+ *
  *    // Example call to reload from original file
  *    oTable.fnReloadAjax();
  */
 
 $.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallback, bStandingRedraw )
 {
-	if ( typeof sNewSource != 'undefined' && sNewSource != null ) {
+	if ( sNewSource !== undefined && sNewSource !== null ) {
 		oSettings.sAjaxSource = sNewSource;
 	}
 
@@ -31,38 +31,37 @@ $.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallba
 	var that = this;
 	var iStart = oSettings._iDisplayStart;
 	var aData = [];
- 
+
 	this.oApi._fnServerParams( oSettings, aData );
-	 
+
 	oSettings.fnServerData.call( oSettings.oInstance, oSettings.sAjaxSource, aData, function(json) {
 		/* Clear the old information from the table */
 		that.oApi._fnClearTable( oSettings );
-		 
+
 		/* Got the data - add it to the table */
 		var aData =  (oSettings.sAjaxDataProp !== "") ?
 			that.oApi._fnGetObjectDataFn( oSettings.sAjaxDataProp )( json ) : json;
-		 
+
 		for ( var i=0 ; i<aData.length ; i++ )
 		{
 			that.oApi._fnAddData( oSettings, aData[i] );
 		}
-		 
+		
 		oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
-		 
-		if ( typeof bStandingRedraw != 'undefined' && bStandingRedraw === true )
+
+		that.fnDraw();
+
+		if ( bStandingRedraw === true )
 		{
 			oSettings._iDisplayStart = iStart;
+			that.oApi._fnCalculateEnd( oSettings );
 			that.fnDraw( false );
 		}
-		else
-		{
-			that.fnDraw();
-		}
-		 
+
 		that.oApi._fnProcessingDisplay( oSettings, false );
-		 
+
 		/* Callback user function - for event handlers etc */
-		if ( typeof fnCallback == 'function' && fnCallback != null )
+		if ( typeof fnCallback == 'function' && fnCallback !== null )
 		{
 			fnCallback( oSettings );
 		}
