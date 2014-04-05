d1d839f6234e19f849995d0e539b9f809ecf61b5
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-04-05 09:12:10 +0100

    Sorting: formatted numbers - add note about default value

--- sorting/formatted-numbers.js
+++ sorting/formatted-numbers.js
@@ -2,6 +2,10 @@
  * This plug-in will provide numeric sorting for numeric columns which have
  * extra formatting, such as thousands separators, currency symbols or any other
  * non-numeric data.
+ * 
+ * By default when a cell is found to have no numeric data its value is sorted
+ * numerically as if its value were 0. This could also be altered to be Inifnity
+ * or -Infinity as required.
  *
  * DataTables 1.10+ has formatted number detection and sorting abilities built-
  * in. As such this plug-in is marked as deprecated, but might be useful when
