dbe9ed3dbff5920ae69ff3e611c6c8128f39e656
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-02-16 11:07:17 +0000

    Update - Sorting: Add `-` as a seperator for eu dates

--- sorting/date-eu.js
+++ sorting/date-eu.js
@@ -23,36 +23,31 @@
 jQuery.extend( jQuery.fn.dataTableExt.oSort, {
 	"date-eu-pre": function ( date ) {
 		date = date.replace(" ", "");
-		var eu_date, year;
 		
-		if (date == '') {
+		if ( ! date ) {
 			return 0;
 		}
 
-		if (date.indexOf('.') > 0) {
-			/*date a, format dd.mn.(yyyy) ; (year is optional)*/
-			eu_date = date.split('.');
-		} else {
-			/*date a, format dd/mn/(yyyy) ; (year is optional)*/
-			eu_date = date.split('/');
-		}
+		var year;
+		var eu_date = date.split(/[\.\-\/]/);
 
 		/*year (optional)*/
-		if (eu_date[2]) {
+		if ( eu_date[2] ) {
 			year = eu_date[2];
-		} else {
+		}
+		else {
 			year = 0;
 		}
 
 		/*month*/
 		var month = eu_date[1];
-		if (month.length == 1) {
+		if ( month.length == 1 ) {
 			month = 0+month;
 		}
 
 		/*day*/
 		var day = eu_date[0];
-		if (day.length == 1) {
+		if ( day.length == 1 ) {
 			day = 0+day;
 		}
 
