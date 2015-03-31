2062f8031e0c909c27b9fbf7643f07f9895369b1
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-03-31 11:43:49 +0100

    Fix #179: Bootstrap table footer section column widths redistributed

--- integration/bootstrap/3/dataTables.bootstrap.js
+++ integration/bootstrap/3/dataTables.bootstrap.js
@@ -21,7 +21,7 @@ $.extend( true, DataTable.defaults, {
 	dom:
 		"<'row'<'col-sm-6'l><'col-sm-6'f>>" +
 		"<'row'<'col-sm-12'tr>>" +
-		"<'row'<'col-sm-6'i><'col-sm-6'p>>",
+		"<'row'<'col-sm-5'i><'col-sm-7'p>>",
 	renderer: 'bootstrap'
 } );
 
