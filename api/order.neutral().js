d1ba8bde45e2ee321055f259d7e654c04addeb7b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-04-02 09:14:08 +0100

    Fix API plugin: `order.neutral()` didn't fully restore the order correctly

--- api/order.neutral().js
+++ api/order.neutral().js
@@ -20,7 +20,11 @@
 $.fn.dataTable.Api.register( 'order.neutral()', function () {
 	return this.iterator( 'table', function ( s ) {
 		s.aaSorting.length = 0;
-		s.aiDisplay.sort();
-		s.aiDisplayMaster.sort();
+		s.aiDisplay.sort( function (a,b) {
+			return a-b;
+		} );
+		s.aiDisplayMaster.sort( function (a,b) {
+			return a-b;
+		} );
 	} );
 } );
