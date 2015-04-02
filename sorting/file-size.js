9c2e407535390fce4557a059591a94b0e0acb91a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-04-02 20:32:26 +0100

    Rewrite file size sorting and type detection plug-ins to use 1.10 style and updated code

--- sorting/file-size.js
+++ sorting/file-size.js
@@ -1,37 +1,36 @@
 /**
  * When dealing with computer file sizes, it is common to append a post fix
- * such as KB, MB or GB to a string in order to easily denote the order of
+ * such as B, KB, MB or GB to a string in order to easily denote the order of
  * magnitude of the file size. This plug-in allows sorting to take these
- * indicates of size into account. A counterpart type detection plug-in 
- * is also available.
+ * indicates of size into account.
+ * 
+ * A counterpart type detection plug-in is also available.
  *
  *  @name File size
- *  @summary Sort abbreviated file sizes correctly (8MB, 4KB etc)
- *  @author _anjibman_
+ *  @summary Sort abbreviated file sizes correctly (8MB, 4KB, etc)
+ *  @author Allan Jardine - datatables.net
  *
  *  @example
- *    $('#example').dataTable( {
+ *    $('#example').DataTable( {
  *       columnDefs: [
  *         { type: 'file-size', targets: 0 }
  *       ]
  *    } );
  */
 
- jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-    "file-size-pre": function ( a ) {
-        var x = a.substring(0,a.length - 2);
+jQuery.fn.dataTable.ext.type.order['file-size-pre'] = function ( data ) {
+    var units = data.replace( /[\d\.]/g, '' ).toLowerCase();
+    var multiplier = 1;
 
-        var x_unit = (a.substring(a.length - 2, a.length) == "MB" ?
-            1000 : (a.substring(a.length - 2, a.length) == "GB" ? 1000000 : 1));
-
-        return parseInt( x * x_unit, 10 );
-    },
-
-    "file-size-asc": function ( a, b ) {
-        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-    },
-
-    "file-size-desc": function ( a, b ) {
-        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    if ( units === 'kb' ) {
+        multiplier = 1000;
     }
-} );
+    else if ( units === 'mb' ) {
+        multiplier = 1000000;
+    }
+    else if ( units === 'gb' ) {
+        multiplier = 1000000000;
+    }
+
+    return parseFloat( data ) * multiplier;
+};
