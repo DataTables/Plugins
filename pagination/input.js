26955db524b05c34cd709ef46fb614393f5d4202
Gordey Doronin <g.doronin@gmail.com>
2015-09-26 08:38:06 +0500

    Missing semicolon added. Additional author added.
    - Missing semicolon added (linting rule).
    - Additional author added (as Allan asked).

--- pagination/input.js
+++ pagination/input.js
@@ -8,6 +8,7 @@
  *  @name Navigation with text input
  *  @summary Shows an input element into which the user can type a page number
  *  @author [Allan Jardine](http://sprymedia.co.uk)
+ *  @author [Gordey Doronin](http://github.com/GDoronin)
  *
  *  @example
  *    $(document).ready(function() {
@@ -220,4 +221,4 @@
 			$(an).children('.' + paginateInputClassName).val(iCurrentPage);
 		}
 	};
-})(jQuery)
+})(jQuery);
