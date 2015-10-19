0f8ec6f7de43780e9c09d2f44517cdec5cac9d17
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-10-19 12:35:08 +0100

    Sorting: Update moment date / time to strip HTML tags before checking for empty
    - Update suggested by sporring - thanks!

--- sorting/datetime-moment.js
+++ sorting/datetime-moment.js
@@ -27,12 +27,17 @@ $.fn.dataTable.moment = function ( format, locale ) {
 
 	// Add type detection
 	types.detect.unshift( function ( d ) {
+		// Strip HTML tags if possible
+		if ( d && d.replace ) {
+			d = d.replace(/<.*?>/g, '') ) {
+		}
+
 		// Null and empty values are acceptable
 		if ( d === '' || d === null ) {
 			return 'moment-'+format;
 		}
 
-		return moment( d.replace ? d.replace(/<.*?>/g, '') : d, format, locale, true ).isValid() ?
+		return moment( d, format, locale, true ).isValid() ?
 			'moment-'+format :
 			null;
 	} );
