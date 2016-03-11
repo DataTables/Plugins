427969e6d9f6db6b356644a77ee1b4c96a9194a3
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-03-11 09:47:55 +0000

    Fix - API: `average()` needs an initial value
    Fixes #257

--- api/average().js
+++ api/average().js
@@ -25,7 +25,7 @@ jQuery.fn.dataTable.Api.register( 'average()', function () {
     var data = this.flatten();
     var sum = data.reduce( function ( a, b ) {
         return (a*1) + (b*1); // cast values in-case they are strings
-    } );
+    }, 0 );
  
     return sum / data.length;
 } );
