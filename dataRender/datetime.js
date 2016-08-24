89cc3a62805f772130de87c5260a091928a426dc
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-08-24 12:25:48 +0100

    Rendering: UMD loader for datetime formatter

--- dataRender/datetime.js
+++ dataRender/datetime.js
@@ -59,7 +59,42 @@
  *    } );
  */
 
-jQuery.fn.dataTable.render.moment = function ( from, to, locale ) {
+
+// UMD
+(function( factory ) {
+	"use strict";
+
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				root = window;
+			}
+
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ?
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}
+(function( $, window, document ) {
+
+
+$.fn.dataTable.render.moment = function ( from, to, locale ) {
 	// Argument shifting
 	if ( arguments.length === 1 ) {
 		locale = 'en';
@@ -78,3 +113,6 @@ jQuery.fn.dataTable.render.moment = function ( from, to, locale ) {
 		return m.format( type === 'sort' || type === 'type' ? 'x' : to );
 	};
 };
+
+
+}));
