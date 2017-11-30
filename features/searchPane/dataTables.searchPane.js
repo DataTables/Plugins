e221ce9c3df00784f13693ffdc580560ccc5e8e6
Allan Jardine <allan@stargazer>
2017-11-30 11:10:26 +0000

    Features - search pane: Cleaning up into functions

--- features/searchPane/dataTables.searchPane.js
+++ features/searchPane/dataTables.searchPane.js
@@ -1,6 +1,5 @@
 // TODO
 // - Tidy up class names
-// - Clean up code into functions
 // - Option to have vertical layout class
 // - Number of horizontal panels class options
 // - Threshold option - require that there is duplicate information in a column before it is used
@@ -8,6 +7,7 @@
 // - Styling for container / header
 // - Styling for clear option
 // - State saving integration
+// - Fix regex characters - (CEO) for example
 
 
 (function( factory ){
@@ -43,92 +43,27 @@ var DataTable = $.fn.dataTable;
 function SearchPanes ( settings, opts ) {
 	var that = this;
 	var table = new DataTable.Api( settings );
-	var container = $('<div class="dt-searchPane"/>')
-		.appendTo( opts.container );
+
+	this.s = {
+		dt: table
+	};
+
+	this.dom = {
+		container: $('<div class="dt-searchPane"/>')
+			.appendTo( opts.container )
+	}
 
 	table.columns(opts.columns).eq(0).each( function ( idx ) {
-		var column = table.column( idx );
-
-		var list = $('<ul/>');
-		var tags = $( column.header() ).hasClass('tags');
-		var bins = that._binData( column.data().flatten(), tags );
-
-		// On initialisation, do we need to set a filtering value from a
-		// saved state or init option?
-		var search = column.search();
-		search = search.length && search[0] ?
-			search[0].substr( 1, search[0].length-2 ).split('|') :
-			[];
-
-		var data = tags ?
-			table.ajax.json().tags :
-			column.data().unique().sort().toArray();
-
-		for ( var i=0, ien=data.length ; i<ien ; i++ ) {
-			if ( data[i] ) {
-				var li = $('<li/>')
-					.html( '<span class="label">'+data[i]+'</span>' )
-					.data( 'filter', data[i] )
-					.append( $('<span class="count" />').html( bins[ data[i] ] ) );
-
-				if ( $.inArray( data[i], search ) !== -1 ) {
-					li.addClass('selected');
-				}
-
-				list.append( li );
-			}
-		}
-
-		$(container).append(
-			$('<div class="pane"/>')
-				.data( 'column', idx )
-				.addClass( search.length ? 'filtering' : '' )
-				.append( '<button type="button" class="close">&times;</button>' )
-				.append( $('<div class="title"/>').html( $(column.header()).text() ) )
-				.append( $('<div class="scroller"/>').append( list ) )
-		);
+		that._pane( idx );
 	} );
 
-	$(container).on( 'click', 'li', function () {
-		if ( $(this).hasClass( 'selected' ) ) {
-			$(this).removeClass( 'selected' );
-		}
-		else {
-			$(this).addClass( 'selected' );
-		}
-
-		var pane = $(this).closest('div.pane');
-		var filters = pane.find( 'li.selected' );
-
-		if ( filters.length === 0 ) {
-			pane.removeClass('filtering');
-			table
-				.column( pane.data('column') )
-				.search('')
-				.draw();
-		}
-		else {
-			pane.addClass('filtering');
-			table
-				.column( pane.data('column') )
-				.search( $.map( filters, function (filter) {
-					return $(filter).data('filter');
-				} ).join('|'), true, false )
-				.draw();
-		}
-	} );
-
-
-	$(container).on( 'click', 'button.close', function () {
-		var pane = $(this).closest('div.pane');
-		pane.find( 'li.selected' ).removeClass( 'selected' );
-		pane.removeClass('filtering');
-
-		table
-			.column( pane.data('column') )
-			.search('')
-			.draw();
-	} );
+	$(this.dom.container)
+		.on( 'click', 'li', function () {
+			that._toggle( this );
+		} )
+		.on( 'click', 'button.close', function () {
+			that._clear( $(this).closest('div.pane') );
+		} );
 }
 
 
@@ -161,6 +96,86 @@ $.extend( SearchPanes.prototype, {
 		} );
 
 		return out;
+	},
+
+	_clear: function ( pane ) {
+		pane.find( 'li.selected' ).removeClass( 'selected' );
+		pane.removeClass('filtering');
+
+		this.s.dt
+			.column( pane.data('column') )
+			.search('')
+			.draw();
+	},
+
+	_pane: function ( idx ) {
+		var table = this.s.dt;
+		var column = table.column( idx );
+		var list = $('<ul/>');
+		var tags = $( column.header() ).hasClass('tags');
+		var bins = this._binData( column.data().flatten(), tags );
+
+		// On initialisation, do we need to set a filtering value from a
+		// saved state or init option?
+		var search = column.search();
+		search = search.length && search[0] ?
+			search[0].substr( 1, search[0].length-2 ).split('|') :
+			[];
+
+		var data = tags ?
+			table.ajax.json().tags :
+			column.data().unique().sort().toArray();
+
+		for ( var i=0, ien=data.length ; i<ien ; i++ ) {
+			if ( data[i] ) {
+				var li = $('<li/>')
+					.html( '<span class="label">'+data[i]+'</span>' )
+					.data( 'filter', data[i] )
+					.append( $('<span class="count" />').html( bins[ data[i] ] ) );
+
+				if ( $.inArray( data[i], search ) !== -1 ) {
+					li.addClass('selected');
+				}
+
+				list.append( li );
+			}
+		}
+
+		$(this.dom.container).append(
+			$('<div class="pane"/>')
+				.data( 'column', idx )
+				.addClass( search.length ? 'filtering' : '' )
+				.append( '<button type="button" class="close">&times;</button>' )
+				.append( $('<div class="title"/>').html( $(column.header()).text() ) )
+				.append( $('<div class="scroller"/>').append( list ) )
+		);
+	},
+	
+	_toggle: function ( li ) {
+		var table = this.s.dt;
+		var li = $(li);
+		var pane = li.closest('div.pane');
+
+		li.toggleClass( 'selected', ! li.hasClass( 'selected' ) );
+
+		var filters = pane.find( 'li.selected' );
+
+		if ( filters.length === 0 ) {
+			pane.removeClass('filtering');
+			table
+				.column( pane.data('column') )
+				.search('')
+				.draw();
+		}
+		else {
+			pane.addClass('filtering');
+			table
+				.column( pane.data('column') )
+				.search( $.map( filters, function (filter) {
+					return $(filter).data('filter');
+				} ).join('|'), true, false )
+				.draw();
+		}
 	}
 } );
 
