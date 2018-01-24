68714b682e488ec975b8ac5adbeec417db1bc217
Shodhan <saveshodhan@gmail.com>
2018-01-25 01:08:00 +0530

    Updating natural-time-detal plugin
    Added support for decades, years, months, milliseconds and microseconds

--- sorting/natural-time-delta.js
+++ sorting/natural-time-delta.js
@@ -21,7 +21,7 @@
 jQuery.extend(jQuery.fn.dataTableExt.oSort,{
     "natural-time-delta-pre" : function(data){
         var result = 0;
-        var pattern = /(\d+\s*weeks?\s*)?(\d+\s*days?\s*)?(\d+\s*hours?\s*)?(\d+\s*minutes?)?(\d+\s*seconds?)?/i
+        var pattern = /(\d+\s*decades?\s*)?(\d+\s*years?\s*)?(\d+\s*months?\s*)?(\d+\s*weeks?\s*)?(\d+\s*days?\s*)?(\d+\s*hours?\s*)?(\d+\s*minutes?\s*)?(\d+\s*seconds?\s*)?(\d+\s*milliseconds?\s*)?(\d+\s*microseconds?\s*)?/i
         var format_time_element = function(el, splitter, mul){
             if (el === undefined){
                 return 0;
@@ -33,11 +33,16 @@ jQuery.extend(jQuery.fn.dataTableExt.oSort,{
         matches.reverse();
 
         var time_elements = [
+            {"splitter" : "us",  "name" : "microsecond",  "mul" : 1 / 1000 / 1000},
+            {"splitter" : "ms",  "name" : "millisecond",  "mul" : 1 / 1000},
             {"splitter" : "s",  "name" : "second",  "mul" : 1},
             {"splitter" : "m",  "name" : "minute",  "mul" : 1 * 60},
             {"splitter" : "h",  "name" : "hour",    "mul" : 1 * 60 * 60},
             {"splitter" : "d",  "name" : "day",     "mul" : 1 * 60 * 60 * 24},
             {"splitter" : "w",  "name" : "week",    "mul" : 1 * 60 * 60 * 24 * 7},
+            {"splitter" : "w",  "name" : "month",    "mul" : 1 * 60 * 60 * 24 * 7 * 30},
+            {"splitter" : "w",  "name" : "year",    "mul" : 1 * 60 * 60 * 24 * 7 * 30 * 12},
+            {"splitter" : "w",  "name" : "decade",    "mul" : 1 * 60 * 60 * 24 * 7 * 30 * 12 * 10},
         ];
 
         time_elements.forEach(function(el, i){
