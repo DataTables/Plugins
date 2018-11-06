86bb873fed68438d6a09c6a1d865ae84fb3dfb4e
Allan Jardine <allan@stargazer>
2018-11-06 09:53:04 +0000

    Sorting: Allow the brackets-negative to work with an empty string (i.e. it being 0)
    Thanks @unclethehornet

--- sorting/brackets-negative.js
+++ sorting/brackets-negative.js
@@ -25,6 +25,10 @@
 		}
 	);
 	$.fn.dataTable.ext.type.order['currency-pre'] = function ( data ) {
+		if ( data === '' ) {
+			return 0;
+		}
+
 		//Check if its in the proper format
 		if(data.match(/[\()]/g)){
 			if( data.match(/[\-]/g) !== true){
