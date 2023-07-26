aa274500d97525901d173b0b6d86a7be20e8677d
Stephen Tomkinson <neonbunny@users.noreply.github.com>
2023-07-26 21:46:36 +0100

    Fix inconsistent whitespace

--- pagination/bootstrap_input.js
+++ pagination/bootstrap_input.js
@@ -10,33 +10,33 @@
  *    } );
  */
 
-$.fn.DataTable.ext.pager.bootstrap_input = function(page, pages){
-   return [ 'first', 'previous', 'input', 'next', 'last' ];
+$.fn.DataTable.ext.pager.bootstrap_input = function (page, pages) {
+    return ['first', 'previous', 'input', 'next', 'last'];
 };
 
 
 main_pageButtonFunc = $.fn.DataTable.ext.renderer.pageButton.bootstrap
 
-$.fn.DataTable.ext.renderer.pageButton = $.extend( true, $.fn.DataTable.ext.renderer.pageButton,
+$.fn.DataTable.ext.renderer.pageButton = $.extend(true, $.fn.DataTable.ext.renderer.pageButton,
     {
-       bootstrap: function ( settings, host, idx, buttons, page, pages ) {
-          main_pageButtonFunc(settings, host, idx, buttons, page, pages);
+        bootstrap: function (settings, host, idx, buttons, page, pages) {
+            main_pageButtonFunc(settings, host, idx, buttons, page, pages);
 
-          input_html = '<div class="input-group">' +
-              '<input style="width: '+ (Math.ceil(Math.log10(pages + 1)) + 2.5) + 'em; margin-left: -1px;" '+ (pages === 1 ? 'disabled ' : '') + 'class="form-control rounded-0" type="number" min="1" max="' + pages + '">' +
-              '<span class="input-group-text rounded-0" id="basic-addon2"> of ' + pages  + '</span>' +
-              '</div>'
+            input_html = '<div class="input-group">' +
+                '<input style="width: ' + (Math.ceil(Math.log10(pages + 1)) + 2.5) + 'em; margin-left: -1px;" ' + (pages === 1 ? 'disabled ' : '') + 'class="form-control rounded-0" type="number" min="1" max="' + pages + '">' +
+                '<span class="input-group-text rounded-0" id="basic-addon2"> of ' + pages + '</span>' +
+                '</div>'
 
-          input_section = $(host).find("[data-dt-idx='input']");
-          input_section.closest("li").prop("onclick", null).off("click");
-		  input_section.closest("li").prop("onkeypress", null).off("keypress");
-          input_section.replaceWith(input_html);
+            let input_section = $(host).find("[data-dt-idx='input']");
+            input_section.closest("li").prop("onclick", null).off("click");
+            input_section.closest("li").prop("onkeypress", null).off("keypress");
+            input_section.replaceWith(input_html);
 
-		   const api = new DataTable.Api(settings);
+            const api = new DataTable.Api(settings);
 
-		   $(host).find("ul.pagination input").val(page + 1).on('change', function(e){
-             api.page(Number($(e.target).val()) - 1).draw('page');
-          });
-       }
+            $(host).find("ul.pagination input").val(page + 1).on('change', function (e) {
+                api.page(Number($(e.target).val()) - 1).draw('page');
+            });
+        }
     }
- )
+)
